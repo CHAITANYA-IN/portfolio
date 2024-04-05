d9ff5e7156f3c602d8167c4b503953cf5efe0c0b
Chaitanya Shinge <chaitanyashinge@gmail.com>
2024-04-05 22:29:48 +0530

    Basic resume component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [
+    AccordionModule
+  ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Resume | Chaitanya Bharat Shinge');
+  }
+
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/pdf/resume/Chaitanya Bharat Shinge.pdf');
+    link.setAttribute('download', 'Chaitanya Bharat Shinge.pdf');
+    link.click();
+    link.remove();
   }
 }
